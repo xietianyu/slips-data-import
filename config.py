19264f686ababa3675c6b72eee0b09360e677c00
xietianyu <806347329@qq.com>
2024-10-22 16:32:31 +0800

    数据导入

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:7p6chllz@mysql-mysql.kb-service.svc.cluster.local:3306/slips-data'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:AAbb1234@10.10.201.13:3306/slips-data'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'QQww1234'
     UPLOAD_FOLDER = 'uploads/'
-    ALLOWED_EXTENSIONS = {'hdf5'}
+    ALLOWED_EXTENSIONS = {'zip'}
+    STORAGE_PATH = '/home/slips/data/config/history'
--- slips_data_svc.py
+++ slips_data_svc.py
@@ -8,6 +8,9 @@ import logging
 from concurrent_log_handler import ConcurrentRotatingFileHandler
 import numpy as np
 from config import Config  # 导入配置文件
+import zipfile
+import tempfile
+import shutil
 
 app = Flask(__name__)
 app.config.from_object(Config)  # 使用配置文件中的参数
@@ -35,8 +38,8 @@ def index():
     return render_template('index.html')
 
 # 处理文件上传和数据导入
-@app.route('/upload', methods=['POST'])
-def upload_file():
+@app.route('/upload/<upload_type>', methods=['POST'])
+def upload_file(upload_type):
     if 'file' not in request.files:
         flash('No file part')
         app.logger.warning('No file part in request')
@@ -54,18 +57,41 @@ def upload_file():
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
         try:
-            import_data_to_db(file_path)
+            handle_uploaded_file(file_path, upload_type)
             app.logger.info(f'File {filename} uploaded and data imported successfully')
             flash('File uploaded and data imported successfully!', 'success')
         except Exception as e:
             flash(f'An error occurred: {e}')
-            app.logger.error(f'Error importing data from file {filename}: {e}')
+            app.logger.error(f'Error storage data from file {filename}: {e}')
         return redirect(url_for('index'))
     else:
         flash('Invalid file type')
         app.logger.warning('Invalid file type')
         return redirect(request.url)
+    
+def handle_uploaded_file(file_path,upload_type):
+    base_path = os.path.join(app.config['STORAGE_PATH'], upload_type)
+    # 清空目标目录的所有文件
+    if os.path.exists(base_path):
+        shutil.rmtree(base_path)
+    os.makedirs(base_path, exist_ok=True)
+    # 根据上传的类型 解压zip文件到STORAGE_PATH中对应上传类型的文件夹中去
+    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+        zip_ref.extractall(base_path)
+    # 遍历文件夹 保证每个文件夹下都有hdf5文件和algo_config.json文件 如果缺失一个文件则提示zip压缩包内哪个文件夹缺失哪个文件 一旦缺少文件 就删除所有文件
+    for root, dirs, files in os.walk(base_path):
+        # 如果dirs下没有hdf5文件或者algo_config.json文件 则删除os.path.join(app.config['STORAGE_PATH'],upload_type)下的所有文件及其子文件夹
+        app.logger.info(f'Processing folder {dirs} in {root}')
+        for subdir in dirs:
+            subdir_path = os.path.join(root, subdir)
+            has_hdf5 = any(file.endswith('.hdf5') for file in os.listdir(subdir_path))
+            has_algo_config = 'algo_config.json' in os.listdir(subdir_path)
 
+            if not has_hdf5 or not has_algo_config:
+                flash(f'An error occurred: {subdir} is missing hdf5 or algo_config.json file')
+                app.logger.error(f'Error: {subdir} is missing hdf5 or algo_config.json file')
+                return
+            
 def import_data_to_db(file_path):
     # 使用 pandas 读取 HDF5 文件中的数据
     with pd.HDFStore(file_path, 'r') as store:
