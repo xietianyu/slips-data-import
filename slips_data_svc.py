1e915fa62d95df744c367c0b6353f41f1ee490d9
xietianyu <806347329@qq.com>
2024-11-12 16:16:49 +0800

    rm db

--- slips_data_svc.py
+++ slips_data_svc.py
@@ -543,6 +543,6 @@ def send_markdown_message(content,mobiles):
 
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
     app.run(host='0.0.0.0', port=5000, debug=True)
